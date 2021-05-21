c468941b1334dc4a7d2120abdd3ff9c81fa984f8
Ivan Valentinov <ivan156drs@gmail.com>
2021-05-21 19:19:08 +0300

    Updated StartPanel class.

--- src/com/company/StartPanel.java
+++ src/com/company/StartPanel.java
@@ -2,25 +2,41 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+
+import static com.company.Main.users;
+import static com.company.Main.currentUserPosition;
 
 public class StartPanel extends JFrame {
 
     // initializing all frames, textFields, buttons, and labels as well as an ImageIcon
     public JFrame frame;
-    private JTextField textName;
-    private JPasswordField textPass;
-    private JButton logIn;
-    private JButton newUser;
-    private JLabel nameLabel;
-    private JLabel passLabel;
+    private JTextField textName, textPass;
+    private JButton logIn, newUser;
+    private JLabel nameLabel, passLabel, imgLabel;
     private ImageIcon topBank;
-    private JLabel imgLabel;
 
-    public StartPanel() { // create a constructor for the starting panel
+    public StartPanel() throws IOException { // create a constructor for the starting panel
         super("FrontPage"); // use the constructor for the superclass JFrame to create a panel
         frame = new JFrame(); // declare the JFrame
 
-        setLayout( new FlowLayout() ); // set the layout to be a flow layout
+        File file = new File("src.dat");
+        if (file.length() != 0) {
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("src.dat"))) {
+                try {
+                    for (; ; ) {
+                        User user = (User) in.readObject();
+                        users.add(user);
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (EOFException e) {
+                    System.out.println("===read all===");
+                }
+            }
+        }
+
+        setLayout(new FlowLayout()); // set the layout to be a flow layout
 
         // set the image icon to be the one of TopBank and then transfer it to a JLabel so we can add it to the frame
         ImageIcon topBank = new ImageIcon("TopBank.png");
@@ -51,11 +67,25 @@ public class StartPanel extends JFrame {
         logIn = new JButton("Log in");
         logIn.setFont(new Font("Serif", Font.ITALIC, 20));
         logIn.setBounds(150, 250, 100, 30);
+        logIn.addActionListener(e -> {
+            for(int i=0;i < users.size(); ++i){
+                if(users.get(i).getUsername().equals(textName.getText()) &&
+                    users.get(i).getPassword().equals(textPass.getText())){
+                    currentUserPosition = i;
+                    System.out.println(currentUserPosition);
+                    new UserHomePage(currentUserPosition);
+                    frame.dispose();
+                }
+            }
+        });
 
         // set the text, font, orientation and position for the new user button
         newUser = new JButton("New User");
         newUser.setFont(new Font("Serif", Font.ITALIC, 20));
         newUser.setBounds(270, 250, 120, 30);
+        newUser.addActionListener(e -> {
+            new NewUser();
+        });
 
         //add everything to the JFrame
         frame.add(imgLabel);
